06cf90188e7571f0bca0e09312094e37f36b0544
Dimitar Dimitrov <dtdimitrov12@gmail.com>
2024-01-24 10:06:56 +0200

    Removed Spring Security. Added a custom authorization validator library

--- src/main/java/com/example/demo/repository/AdmissionRepository.java
+++ src/main/java/com/example/demo/repository/AdmissionRepository.java
@@ -1,12 +1,12 @@
 package com.example.demo.repository;
 
+import com.example.authorizationValidator.repository.AuthenticationEntityRepository;
 import com.example.demo.entity.Admission;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
-public interface AdmissionRepository extends JpaRepository<Admission, Long> {
+public interface AdmissionRepository extends AuthenticationEntityRepository<Admission, Long> {
     Optional<Admission> findByUsername(String username);
 
     @Query("""
